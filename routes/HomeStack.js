492265b00148a81cbf35c6f33cf5db5f6616fdfd
khunamir <amir12102009@gmail.com>
2021-07-21 00:58:03 +0800

    Custom nav bar and Menu

--- routes/HomeStack.js
+++ routes/HomeStack.js
@@ -1,12 +1,18 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/home';
 import DetailsScreen from '../screens/details';
+import CustomNavigationBar from '../shared/customNavigationBar';
 
 const Stack = createStackNavigator();
 
 export default function HomeStack() {
     return (
-        <Stack.Navigator initialRouteName="Home">
+        <Stack.Navigator 
+            initialRouteName="Home"
+            screenOptions={{
+                header: (props) => <CustomNavigationBar {...props} />,
+            }}
+        >
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Details" component={DetailsScreen} />
         </Stack.Navigator>
--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import {globalStyles} from '../styles/globalStyles';
 
 export default function HomeScreen() {
   return (
     <View style={globalStyles.container}>
       <Text>Home Screen</Text>
+      <Button
+        title="Go to details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
